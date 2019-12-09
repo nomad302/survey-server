093fed2e1fa024f815ea50e1dddff406a45671d3
nomad302 <sagar.nargundi@gmail.com>
2019-12-09 07:10:56 +0530

    code refactor is done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express"); // common js modules to import the libs
+require("./services/passport");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+require("./routes/authRoute")(app);
 
 const PORT = process.env.PORT || 5000;
 
